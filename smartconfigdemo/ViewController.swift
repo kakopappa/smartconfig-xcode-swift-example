5f2a1edde7aa4f1582fd3f5f90286044c044552f
Aruna Tennakoon <aruna@box24corp.com>
2020-07-17 09:26:08 +0700

    ios13 support added

--- smartconfigdemo/ViewController.swift
+++ smartconfigdemo/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        self.startSmartConfig()
+        //self.startSmartConfig()
     }
     
     func startSmartConfig() {
